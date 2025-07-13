9d343b7287a91ac0c77a0bc44ae478a1fbde8114
Karl-Andero Mere <anddero@gmail.com>
2025-07-13 14:53:19 +0300

    Make a class for Stock asset holding.

--- src/portfolio.js
+++ src/portfolio.js
@@ -13,3 +13,25 @@ class CashHolding {
         return this.value;
     }
 }
+
+class StockHolding {
+    constructor(code, friendlyName, currency) {
+        this.code = code;
+        this.friendlyName = friendlyName;
+        this.currency = currency;
+        this.shares = new Decimal(0);
+    }
+
+    getCurrency() {
+        return this.currency;
+    }
+
+    getCurrentShares() {
+        return this.shares;
+    }
+
+    updateShares(diff) {
+        this.shares = this.shares.plus(diff);
+        return this.shares;
+    }
+}
