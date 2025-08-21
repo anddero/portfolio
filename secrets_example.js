807614af4025aba449fd5dd8bd2283675c5b0c1f
Karl-Andero Mere <anddero@gmail.com>
2025-08-21 17:44:34 +0300

    Add configuration file for API keys.

---
+++ secrets_example.js
@@ -0,0 +1,4 @@
+window.APP_CONFIG = {
+    fmpApiKey: '', // Finance Modeling Prep API key, optional. If not provided, service will not be fetched.
+    avApiKey: '', // Alpha Vantage API key, optional. If not provided, service will not be fetched.
+};
